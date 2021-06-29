58bfe8162d968264bc5f9c30fc1097217fddf89b
Justin Hager <developerjustinhager@pm.me>
2021-06-29 09:18:03 -0700

    add model instance remove test

---
+++ test/delete_test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const User = require('../src/models/User');
+
+describe('Deleting a user', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save().then(() => {
+      done();
+    });
+  });
+
+  it('model instance remove', (done) => {
+    joe
+      .remove()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(!user);
+        done();
+      });
+  });
+
+  it('class method remove', () => {});
+
+  it('class method findAndRemove', () => {});
+
+  it('class method findByIdAndRemove', () => {});
+});
